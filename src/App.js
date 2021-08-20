1f288af2e89f83229111f4494748ef4c7fd146bc
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-08-20 18:42:58 -0500

    Edit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,57 @@
-import App from './Home'
+import React, { useState, useEffect } from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
+import Form from './Form'
+import Home from './Home'
+import Pizza from './Pizza'
+import PizzasList from './PizzasList'
+import './App.css';
+import data from './data'
+
+function fetchStock() {
+  
+  return Promise.resolve({ success: true, data })
+}
+
+function App() {
+
+  const [stock, setStock] = useState([])
+
+  useEffect(() => {
+    fetchStock().then(res => setStock(res.data))
+  }, [])
+
+  return (
+    <div className="App">
+      <nav>
+        <h1 className='store-header'>Lambda Eats</h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/pizzas-list'>Pizza</Link>
+        </div>
+      </nav>
+
+    <Switch>
+
+      <Route path={'/pizzas-list/:pizzaID'}>
+        <Pizza pizzas={stock}/>
+      </Route>
+
+      <Route path='/pizzas-list'>
+        <PizzasList pizzas={stock}/>
+      </Route>
+
+      <Route path='/'>
+        <Home />
+      </Route>
+    
+      <Route path='/pizza'>
+        <Form />
+      </Route>
+         
+    </Switch>
+     
+    </div>
+  );
+}
 
 export default App;
--- src/Home.js
+++ src/Home.js
@@ -1,63 +1,8 @@
+import React from 'react'
 import { useHistory } from 'react-router-dom'
-import React, { useState, useEffect } from 'react'
-import { Route, Link, Switch } from 'react-router-dom'
-import Form from './Form'
-import Pizza from './Pizza'
-import PizzasList from './PizzasList'
-import './App.css';
-import data from './data'
 
-function fetchStock() {
-  // fetchStock simulates getting data through axios.get(<URL>)
-  return Promise.resolve({ success: true, data })
-}
 
-export default function App() {
-
-  const [stock, setStock] = useState([])
-
-  useEffect(() => {
-    fetchStock().then(res => setStock(res.data))
-  }, [])
-
-  return (
-    <div className="App">
-    <nav>
-        <h1 className='store-header'>Lambda Eats</h1>
-        <div className='nav-links'>
-          
-          <Link to='/'>Home</Link>
-          <Link to='/pizzas-list'>Pizza</Link>
-        </div>
-      </nav>
-
-    
-      <Switch>
-
-      <Route path={'/pizzas-list/:pizzaID'}>
-        <Pizza pizzas={stock}/>
-      </Route>
-
-      <Route path='/pizzas-list'>
-        <PizzasList pizzas={stock}/>
-      </Route>
-
-      <Route path='/'>
-        <Home />
-      </Route>
-
-    
-      <Route path='/pizza'>
-        <Form />
-      </Route>
-         
-      </Switch>
-     
-    </div>
-  );
-}
-
-function Home() {
+export default function Home() {
     const history = useHistory()
     const routeToShop = () => {
     console.log(history)
