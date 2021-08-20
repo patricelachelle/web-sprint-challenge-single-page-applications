67b23ef3a184f7ad3357623ebbdd76708f42b0ca
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-08-20 15:59:42 -0500

    Edit

---
+++ src/data/index.js
@@ -0,0 +1,35 @@
+export default [
+    {
+        name: 'sausage',
+        id: 1,
+        imageUrl: 'https://media.istockphoto.com/photos/sausage-pizza-picture-id968775256',
+        description: 'sausage',
+        price: 20.00,
+        
+    },
+    {
+        name: 'pepperoni',
+        id: 2,
+        imageUrl: 'https://www.simplyrecipes.com/thmb/HQJVIRwkySb-QiYv6saC9dEjU5A=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2019__09__easy-pepperoni-pizza-lead-4-82c60893fcad4ade906a8a9f59b8da9d.jpg',
+        description: 'pepperoni',
+        price: 20.00,
+        
+    },
+    {
+        name: 'cheese',
+        id: 3,
+        imageUrl: 'https://i0.wp.com/www.eatthis.com/wp-content/uploads/2020/05/cheese-pizza.jpg?resize=640%2C360&ssl=1',
+        description: 'cheese',
+        price: 20.00,
+        
+    },
+    {
+        name: 'veggies',
+        id: 4,
+        imageUrl: 'https://media.istockphoto.com/photos/homemade-veggie-pizza-with-mushrooms-peppers-picture-id842082336',
+        description: 'veggie',
+        price: 20.00,
+        
+    }
+    
+]
