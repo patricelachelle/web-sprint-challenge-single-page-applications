588ee96325f8fc87d425d491846423b98d21d06c
patricelachelle <79610155+patricelachelle@users.noreply.github.com>
2021-08-20 14:51:30 -0500

    Edit

--- src/PizzasList.js
+++ src/PizzasList.js
@@ -1,23 +1,28 @@
-// import React from 'react'
-// import { Link, useRouteMatch } from 'react-router-dom'
+import React from 'react'
+import { Link, useRouteMatch } from 'react-router-dom'
 
-// export default function ItemsList(props) {
-//   const { pizzas } = props
-//   const { url } = useRouteMatch()
+export default function ItemsList(props) {
+  const { pizzas } = props
+  const { url } = useRouteMatch()
 
-//   return (
-//     <div className='items-list-wrapper'>
-      
-//           <Link to={`${url}/${pizza.id}`}>
-//           <img
-//             className='items-list-image'
-//             src={pizza.imageUrl}
-//             alt={pizza.name}
-//           />
-//           <p>{pizza.name}</p>
-//           </Link>
-//           <p>{pizza.price}</p>
-        
-//     </div>
-//   )
-// }
+  return (
+    <div className='items-list-wrapper'>
+      {pizzas.map(pizza => (
+        <div
+          className='item-card'
+          key={pizza.id}
+        >
+          <Link to={`${url}/${pizza.id}`}>
+          <img
+            className='items-list-image'
+            src={pizza.imageUrl}
+            alt={pizza.name}
+          />
+          <p>{pizza.name}</p>
+          </Link>
+          <p>{pizza.price}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
